45b28bf3c3725b2c700beb38afd528d473c7dc2b
Hannah Shiels <thebelfastflygon@gmail.com>
2020-02-05 16:19:34 +0000

    Add files via upload

--- project-024/js/index.js
+++ project-024/js/index.js
@@ -4,6 +4,7 @@ let answers = document.querySelectorAll('.answer');
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function(e) {
         e.preventDefault();
+        btns[i].classList.toggle('rotate');
         let parent = btns[i].parentElement;
         let sibling = parent.nextElementSibling;
         sibling.classList.toggle('active');
