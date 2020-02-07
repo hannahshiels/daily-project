f1aa196db7494629ba2f6cfed3118c1fc7e9ec30
Hannah Shiels <thebelfastflygon@gmail.com>
2020-02-07 17:18:09 +0000

    Fix js to take correct input

--- project-026/js/index.js
+++ project-026/js/index.js
@@ -5,9 +5,7 @@ btn.addEventListener('click', calculateBMI);
 function calculateBMI(e) {
     e.preventDefault();
     let height = document.querySelector('#height').value;
-    height = parseFloat(height);
     let weight = document.querySelector('#weight').value;
-    weight = parseFloat(weight);
     let bmi = weight / Math.pow(height, 2);
     bmi = bmi.toFixed(2);
     h2.textContent = `Your BMI is ${ bmi }`;
