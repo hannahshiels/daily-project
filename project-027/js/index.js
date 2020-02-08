060778aece9390e09c5092ff75b6e9c0faf45bbd
Hannah Shiels <thebelfastflygon@gmail.com>
2020-02-08 16:57:39 +0000

    Check if all balloons are popped

--- project-027/js/index.js
+++ project-027/js/index.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.container');
 const colors = ['red', 'blue', 'yellow', 'green'];
-
+const h1 = document.querySelector('h1');
+let numberofItems = 42;
+let j = 0;
 // create a balloon
 const balloon = document.createElement('div');
 balloon.classList.add('item');
@@ -11,7 +13,7 @@ function randomColor() {
 }
 
 // insert balloons into container
-for (let i = 0; i < 42; i++) {
+for (let i = 0; i < numberofItems; i++) {
     container.appendChild(balloon.cloneNode());
 }
 
@@ -21,5 +23,14 @@ for (let i = 0; i < items.length; i++) {
     items[i].style.backgroundColor = colors[randomColor()];
     items[i].addEventListener('mouseover', function() {
         items[i].classList.add('invisible');
+        j++;
+        checkPopped(j);
     })
+}
+
+function checkPopped(j) {
+    if (j == numberofItems) {
+        h1.textContent = 'You popped them all! Reload to play again!'
+    }
+
 }
