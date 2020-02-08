99275034bfd0ac7938355e7250a4ea4489e544b2
Hannah Shiels <thebelfastflygon@gmail.com>
2020-02-08 16:43:27 +0000

    Optimised script

--- project-027/js/index.js
+++ project-027/js/index.js
@@ -1,22 +1,25 @@
 const container = document.querySelector('.container');
+const colors = ['red', 'blue', 'yellow', 'green'];
+
+// create a balloon
 const balloon = document.createElement('div');
 balloon.classList.add('item');
-const colors = ['red', 'blue', 'yellow', 'green'];
-balloon.style.backgroundColor = colors[randomColor()];
+
 
 function randomColor() {
     return Math.floor(Math.random() * colors.length);
 }
 
+// insert balloons into container
 for (let i = 0; i < 42; i++) {
     container.appendChild(balloon.cloneNode());
-    balloon.style.backgroundColor = colors[randomColor()];
 }
 
 const items = document.querySelectorAll('.item');
-
+// make balloons disappear on mouseover
 for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener('click', function() {
-        items[i].remove();
+    items[i].style.backgroundColor = colors[randomColor()];
+    items[i].addEventListener('mouseover', function() {
+        items[i].classList.add('invisible');
     })
 }
