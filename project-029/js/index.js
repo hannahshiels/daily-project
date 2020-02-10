3ff2632cf6ce24e400932987079572af73b40f68
Hannah Shiels <thebelfastflygon@gmail.com>
2020-02-10 17:59:05 +0000

    Add files via upload

--- project-029/js/index.js
+++ project-029/js/index.js
@@ -1,5 +1,15 @@
 const btn = document.querySelector('.btn');
 const searchBar = document.querySelector('.search-bar');
-btn.addEventListener('click', function() {
-    searchBar.classList.toggle('visible');
+document.addEventListener('keypress', function(e) {
+    if (e.keyCode == 87) {
+        showNav();
+    } else {
+        console.log(e.keyCode)
+    }
 })
+btn.addEventListener('click', showNav)
+
+function showNav() {
+    searchBar.classList.toggle('visible');
+
+}
