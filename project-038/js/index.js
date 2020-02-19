d007e324d44319402a67864e5379a31ae8347333
Hannah Shiels <thebelfastflygon@gmail.com>
2020-02-19 16:48:02 +0000

    Add files via upload

---
+++ project-038/js/index.js
@@ -0,0 +1,26 @@
+const h2 = document.querySelector('#output');
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', scrambleWord);
+
+function scrambleWord() {
+    h2.classList.remove('visible');
+    const input = document.querySelector('#userInput').value;
+    let wordArr = input.split('');
+    let newArr = [];
+    let randomNum;
+    let checkArr = input.split(' ');
+    if (checkArr.length === 1) {
+        wordArr.forEach(element => {
+            randomNum = Math.floor(Math.random() * 2);
+            if (randomNum === 1) {
+                newArr.unshift(element);
+            } else {
+                newArr.push(element);
+            }
+        });
+        h2.textContent = newArr.join('');
+    } else if (checkArr > 1) {
+        checkArr.pop();
+    }
+
+}
